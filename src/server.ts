77524046e8871832a4ca1ca201eef737d5682b05
Gabriela Silva <gabriela.silva1991@outlook.com>
2021-04-24 21:48:56 -0300

    migration, entities, repository, controller e services adicionados (referentes a Users/Messages)

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,8 @@
-import express from 'express';
+import express from "express";
 
-import './database';
+import "./database";
 
-import { routes } from './routes';
+import { routes } from "./routes";
 
 const app = express();
 
@@ -10,17 +10,17 @@ app.use(express.json());
 
 app.use(routes);
 
-app.listen(3333, () => console.log('Server is running on port 3333'));
+app.listen(3333, () => console.log("Server is running on port 3333"));
 
 /* 
-app.get(('/'), (request, response) => {
-  //return response.send('Olá NLW 05')
+app.get(("/"), (request, response) => {
+  //return response.send("Olá NLW 05")
   //mesma coisa de 
   return response.json({
-    message:'Olá NLW 05'
+    message:"Olá NLW 05"
   })
 });
-app.post(('/'), (request, response) => {
+app.post(("/"), (request, response) => {
   return response.json({
     message:"usuário criado com sucesso"
   })
